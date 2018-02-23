91bd3290331fad6affb5da0f493229ef0d3a5286
Nick Harrison <harrisna@uwec.edu>
2018-02-23 16:26:13 -0600

    find client and server ip on start

--- src/net.h
+++ src/net.h
@@ -1,7 +1,11 @@
 #include <stdlib.h>
+#include <stdint.h>
 
 extern int startServer();
 extern int startClient(char* host);
 extern int acceptClient(int socket);
 extern void net_write(void* x, size_t sz, int socket);
 extern void net_read(void* x, size_t sz, int socket);
+extern uint32_t net_getaddr(int socket);
+extern uint32_t net_getlocaladdr(int socket);
+extern void net_addrstr(uint32_t addr, char *str, size_t len);
